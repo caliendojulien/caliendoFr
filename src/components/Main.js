b54b3721988b4df34ce350c086fa23751bc8c121
Caliendo Julien <34681928+caliendojulien@users.noreply.github.com>
2020-09-14 19:14:49 +0200

    Modification des réseaux sociaux

--- src/components/Main.js
+++ src/components/Main.js
@@ -145,25 +145,15 @@ class Main extends React.Component {
           <ul className="icons">
             <li>
               <a
-                href="https://twitter.com/HuntaroSan"
+                href="https://twitter.com/caliendojulien"
                 className="icon fa-twitter"
               >
                 <span className="label">Twitter</span>
               </a>
             </li>
             <li>
-              <a href="https://codebushi.com" className="icon fa-facebook">
-                <span className="label">Facebook</span>
-              </a>
-            </li>
-            <li>
-              <a href="https://codebushi.com" className="icon fa-instagram">
-                <span className="label">Instagram</span>
-              </a>
-            </li>
-            <li>
               <a
-                href="https://github.com/codebushi/gatsby-starter-dimension"
+                href="https://github.com/caliendojulien"
                 className="icon fa-github"
               >
                 <span className="label">GitHub</span>
